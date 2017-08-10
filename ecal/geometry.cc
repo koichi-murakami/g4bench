296e73b65adac67381c5b82e902670ec344df49e
Koichi Murakami <koichi.murakami@kek.jp>
2017-08-10 18:54:20 +0900

    skelton

---
+++ ecal/geometry.cc
@@ -0,0 +1,10 @@
+/*============================================================================
+Copyright 2017 Koichi Murakami
+
+Distributed under the OSI-approved BSD License (the "License");
+see accompanying file LICENSE for details.
+
+This software is distributed WITHOUT ANY WARRANTY; without even the
+implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+See the License for more information.
+============================================================================*/
