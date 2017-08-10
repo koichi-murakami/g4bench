05a675ca532d1c8214d3fba5711bf3274af3db5f
Koichi Murakami <koichi.murakami@kek.jp>
2017-08-10 18:46:27 +0900

    add version file

---
+++ version.h
@@ -0,0 +1,23 @@
+/*============================================================================
+  Copyright 2017 Koichi Murakami
+
+  Distributed under the OSI-approved BSD License (the "License");
+  see accompanying file LICENSE for details.
+
+  This software is distributed WITHOUT ANY WARRANTY; without even the
+  implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+  See the License for more information.
+============================================================================*/
+#ifndef VERSION_H_
+#define VERSION_H_
+
+#define G4BENCH_VERSION_MAJOR "1"
+#define G4BENCH_VERSION_MINOR "0d1"
+#define G4BENCH_VERSION_BUILD 0xcb5e724
+
+namespace {
+  const int build_head = (G4BENCH_VERSION_BUILD & 0xffff000) >> 12;
+  const int build_tail = G4BENCH_VERSION_BUILD & 0xfff;
+} // namespace
+
+#endif
