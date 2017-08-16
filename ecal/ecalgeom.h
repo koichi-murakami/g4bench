895e07e637da828b7c662d0a256b5e82594592ea
Koichi Murakami <koichi.murakami@kek.jp>
2017-08-16 18:11:20 +0900

    updated

--- ecal/ecalgeom.h
+++ ecal/ecalgeom.h
@@ -8,3 +8,19 @@ This software is distributed WITHOUT ANY WARRANTY; without even the
 implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 See the License for more information.
 ============================================================================*/
+#ifndef ECAL_GEOM_H_
+#define ECAL_GEOM_H_
+
+#include "G4VUserDetectorConstruction.hh"
+
+class QDetectorConstruction : public G4VUserDetectorConstruction {
+
+public:
+  QDetectorConstruction();
+  ~QDetectorConstruction();
+
+  virtual G4VPhysicalVolume* Construct();
+
+};
+
+#endif
