4602b16709c91843391bf2c768730067c10557a3
Koichi Murakami <koichi.murakami@kek.jp>
2017-08-25 05:23:32 +0900

    implement pb-sc cal

--- hcal/hcalgeom.cc
+++ hcal/hcalgeom.cc
@@ -12,119 +12,95 @@ See the License for more information.
 #include "G4LogicalVolume.hh"
 #include "G4NistManager.hh"
 #include "G4PVPlacement.hh"
+#include "G4PVReplica.hh"
 #include "G4SystemOfUnits.hh"
 #include "G4Tubs.hh"
 #include "G4VisAttributes.hh"
-#include "ecalgeom.h"
 #include "calscorer.h"
+#include "hcalgeom.h"
 
 // --------------------------------------------------------------------------
-EcalGeom::EcalGeom()
+HcalGeom::HcalGeom()
 {
 }
 
 // --------------------------------------------------------------------------
-EcalGeom::~EcalGeom()
+HcalGeom::~HcalGeom()
 {
 }
 
 // --------------------------------------------------------------------------
-G4VPhysicalVolume* EcalGeom::Construct()
+G4VPhysicalVolume* HcalGeom::Construct()
 {
   G4NistManager* nist_manager = G4NistManager::Instance();
 
   // world volume
-  const double kDXYZ_World = 100.*cm;
-  G4Box* world_box = new G4Box("world", kDXYZ_World/2.,
-                                        kDXYZ_World/2., kDXYZ_World/2.);
+  const double kDXY_World = 100.*cm;
+  const double kDZ_World = 200.*cm;
+  G4Box* world_box = new G4Box("world", kDXY_World/2.,
+                                        kDXY_World/2., kDZ_World/2.);
 
   G4Material* air = nist_manager-> FindOrBuildMaterial("G4_AIR");
   G4LogicalVolume* world_lv = new G4LogicalVolume(world_box, air, "world");
 
   G4PVPlacement* world_pv = new G4PVPlacement(nullptr, G4ThreeVector(), "world",
-                                              world_lv, 0, false, 0);
+                                              world_lv, nullptr, false, 0);
 
-  // voxel
-  const double kDXZ_Voxel = 20.*mm;
-  const double kDL_Voxel = 60.*cm;
-
-  G4Box* voxel_box = new G4Box("voxel", kDXZ_Voxel/2., kDL_Voxel/2.,
-                                        kDXZ_Voxel/2.);
-  G4LogicalVolume* voxel_lv = new G4LogicalVolume(voxel_box, air, "voxel");
-
-  const int kNzTracker = 5;
-  const int kNxTracker = 11;
-  const double kZTracker = -20.*cm;
-  int index = 0;
-  for (auto iz = 0; iz < kNzTracker; iz++) {
-    for (auto ix = -kNxTracker; ix <= kNxTracker; ix++) {
-      double x0 = kDXZ_Voxel*ix;
-      double z0 = kZTracker + kDXZ_Voxel*iz;
-      G4PVPlacement* pvoxel =
-        new G4PVPlacement(0, G4ThreeVector(x0, 0., z0), voxel_lv,
-                          "voxel", world_lv, false, index);
-      index++;
-    }
-  }
-
-  // tube tracker
-  const double kTubeInner = 8.5*mm;
-  const double kTubeOuter = 9.5*mm;
-  G4Tubs* tube = new G4Tubs("tube", kTubeInner, kTubeOuter, kDL_Voxel/2.,
-                                        0., 360.*deg);
-
-  G4Material* al = nist_manager-> FindOrBuildMaterial("G4_Al");
-  G4LogicalVolume* tube_lv = new G4LogicalVolume(tube, al, "tube");
-
-  G4RotationMatrix* tube_rm = new G4RotationMatrix;
-  tube_rm-> rotateX(-90.*deg);
-  G4PVPlacement* tube_pv =
-    new G4PVPlacement(tube_rm, G4ThreeVector(), tube_lv, "tube",
-                      voxel_lv, false, 0);
-
-  // calorimeter
-  const double kDXY_Cal = 25.*mm;
-  const double kDZ_Cal = 30.*cm;
+  // s/w calorimeter
+  const double kDXY_Cal = 30.*cm;
+  const double kDZ_Cal = 100.*cm;
 
   G4Box* cal_box = new G4Box("cal", kDXY_Cal/2., kDXY_Cal/2., kDZ_Cal/2.);
-  G4Material* csi = nist_manager-> FindOrBuildMaterial("G4_CESIUM_IODIDE");
+  G4Material* pb = nist_manager-> FindOrBuildMaterial("G4_Pb");
 
-  G4LogicalVolume* cal_lv = new G4LogicalVolume(cal_box, csi, "cal");
+  G4LogicalVolume* cal_lv = new G4LogicalVolume(cal_box, pb, "cal");
 
-  index = 0;
-  const int kNxyCal = 11;
-  const double kZ_Cal = 20.*cm;
-  for (auto ix = -kNxyCal; ix <= kNxyCal; ix++) {
-    for (auto iy = -kNxyCal; iy <= kNxyCal; iy++) {
-      double x0 = kDXY_Cal*ix;
-      double y0 = kDXY_Cal*iy;
-      G4PVPlacement* cal_pv =
-        new G4PVPlacement(0, G4ThreeVector(x0, y0, kZ_Cal),
-                          cal_lv, "cal", world_lv, false, index);
-      index++;
-    }
-  }
+  G4PVPlacement* cal_pv = new G4PVPlacement(nullptr, G4ThreeVector(),
+                                cal_lv, "cal", world_lv, false, 0);
+
+  // replication along z-axis
+  const double kDZ_Pb = 2.*cm; // 1(Pb)+1(scinti)
+  G4Box* pb_solid = new G4Box("pb", kDXY_Cal/2., kDXY_Cal/2., kDZ_Pb/2.);
+
+  G4LogicalVolume* pb_lv = new G4LogicalVolume(pb_solid, pb, "pb");
+
+  const int kNZ = 50;
+  G4PVReplica* pb_rep = new G4PVReplica("pb", pb_lv, cal_lv, kZAxis,
+                                        kNZ, kDZ_Pb);
+
+  // scintillator
+  const double kDZ_SC = 1.*cm;
+  G4Box* sc_solid = new G4Box("sc", kDXY_Cal/2., kDXY_Cal/2., kDZ_SC/2.);
+  G4Material* sc =
+    nist_manager-> FindOrBuildMaterial("G4_PLASTIC_SC_VINYLTOLUENE");
+
+  G4LogicalVolume* sc_lv = new G4LogicalVolume(sc_solid, sc, "sc");
+
+  G4PVPlacement* sc_pv =
+    new G4PVPlacement(nullptr, G4ThreeVector(0.,0.,kDZ_SC/2.),
+                      sc_lv, "sc", pb_lv, false, 0);
 
   CalScorer* cal_scorer = new CalScorer();
   cal_scorer-> SetSimData(simdata_);
-  cal_lv-> SetSensitiveDetector(cal_scorer);
+  sc_lv-> SetSensitiveDetector(cal_scorer);
 
   // vis attributes
   G4VisAttributes* va = nullptr;
-  va = new G4VisAttributes(G4Color(1.,1.,1.));
+  va = new G4VisAttributes(G4Color(1., 1., 1.));
   va-> SetVisibility(true);
   world_lv-> SetVisAttributes(va);
 
-  va= new G4VisAttributes(G4Color(0.,0.8,0.8));
+  va = new G4VisAttributes(G4Color(1., 1., 1.));
   va-> SetVisibility(false);
-  voxel_lv-> SetVisAttributes(va);
-
-  va= new G4VisAttributes(G4Color(0.,0.8,0.8));
-  va-> SetVisibility(true);
-  tube_lv-> SetVisAttributes(va);
-
-  va= new G4VisAttributes(G4Color(0.5,0.5,0.));
   cal_lv-> SetVisAttributes(va);
+
+  va= new G4VisAttributes(G4Color(0.5, 0.5, 0.));
+  //va-> SetForceSolid(true);
+  pb_lv-> SetVisAttributes(va);
+
+  va= new G4VisAttributes(G4Color(0., 0.5, 0.5));
+  va-> SetForceSolid(true);
+  sc_lv-> SetVisAttributes(va);
 
   return world_pv;
 }
