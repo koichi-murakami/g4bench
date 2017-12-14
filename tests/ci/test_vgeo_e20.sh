c411ab1bd7035ffba444664df35a402a1dcd0762
Koichi Murakami <koichi.murakami@kek.jp>
2017-12-14 18:35:51 +0900

    migrate test set for v10.4.0

--- tests/ci/test_vgeo_e20.sh
+++ tests/ci/test_vgeo_e20.sh
@@ -29,16 +29,16 @@ check_error
 show_line
 echo "@@ Run a program..."
 #
-export G4NEUTRONHPDATA=/opt/geant4/data/10.3.3/G4NDL4.5
-export G4LEDATA=/opt/geant4/data/10.3.3/G4EMLOW6.50
-export G4LEVELGAMMADATA=/opt/geant4/data/10.3.3/PhotonEvaporation4.3.2
-export G4RADIOACTIVEDATA=/opt/geant4/data/10.3.3/RadioactiveDecay5.1.1
-export G4NEUTRONXSDATA=/opt/geant4/data/10.3.3/G4NEUTRONXS1.4
-export G4PIIDATA=/opt/geant4/data/10.3.3/G4PII1.3
-export G4REALSURFACEDATA=/opt/geant4/data/10.3.3/RealSurface1.0
-export G4SAIDXSDATA=/opt/geant4/data/10.3.3/G4SAIDDATA1.1
-export G4ABLADATA=/opt/geant4/data/10.3.3/G4ABLA3.0
-export G4ENSDFSTATEDATA=/opt/geant4/data/10.3.3/G4ENSDFSTATE2.1
+export G4NEUTRONHPDATA=/opt/geant4/data/10.4.0/G4NDL4.5
+export G4LEDATA=/opt/geant4/data/10.4.0/G4EMLOW7.3
+export G4LEVELGAMMADATA=/opt/geant4/data/10.4.0/PhotonEvaporation5.2
+export G4RADIOACTIVEDATA=/opt/geant4/data/10.4.0/RadioactiveDecay5.2
+export G4NEUTRONXSDATA=/opt/geant4/data/10.4.0/G4NEUTRONXS1.4
+export G4PIIDATA=/opt/geant4/data/10.4.0/G4PII1.3
+export G4REALSURFACEDATA=/opt/geant4/data/10.4.0/RealSurface2.1
+export G4SAIDXSDATA=/opt/geant4/data/10.4.0/G4SAIDDATA1.1
+export G4ABLADATA=/opt/geant4/data/10.4.0/G4ABLA3.1
+export G4ENSDFSTATEDATA=/opt/geant4/data/10.4.0/G4ENSDFSTATE2.2
 #
 cat << EOD > config.json5
 {
