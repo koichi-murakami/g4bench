5c229f955772dbe0e851e2ea07397a53c4544ea3
Koichi Murakami <koichi.murakami@kek.jp>
2018-01-10 15:53:11 +0900

    release v1.2.1

--- version.h
+++ version.h
@@ -12,8 +12,8 @@
 #define VERSION_H_
 
 #define G4BENCH_VERSION_MAJOR "1"
-#define G4BENCH_VERSION_MINOR "2.0"
-#define G4BENCH_VERSION_BUILD 0x4a9527d
+#define G4BENCH_VERSION_MINOR "2.1"
+#define G4BENCH_VERSION_BUILD 0x1dac0b2
 
 namespace {
   const int build_head = (G4BENCH_VERSION_BUILD & 0xffff000) >> 12;
