a033c597859762b46667ef22c14b28d0bba747e4
Koichi Murakami <koichi.murakami@kek.jp>
2018-03-13 16:42:05 +0900

    change env variable order in alphabetically

--- tests/ci/test_hcal.sh
+++ tests/ci/test_hcal.sh
@@ -29,16 +29,16 @@ check_error
 show_line
 echo "@@ Run a program..."
 #
-export G4NEUTRONHPDATA=/opt/geant4/data/10.4.0/G4NDL4.5
+export G4ABLADATA=/opt/geant4/data/10.4.0/G4ABLA3.1
 export G4LEDATA=/opt/geant4/data/10.4.0/G4EMLOW7.3
-export G4LEVELGAMMADATA=/opt/geant4/data/10.4.0/PhotonEvaporation5.2
-export G4RADIOACTIVEDATA=/opt/geant4/data/10.4.0/RadioactiveDecay5.2
+export G4ENSDFSTATEDATA=/opt/geant4/data/10.4.0/G4ENSDFSTATE2.2
+export G4NEUTRONHPDATA=/opt/geant4/data/10.4.0/G4NDL4.5
 export G4NEUTRONXSDATA=/opt/geant4/data/10.4.0/G4NEUTRONXS1.4
 export G4PIIDATA=/opt/geant4/data/10.4.0/G4PII1.3
-export G4REALSURFACEDATA=/opt/geant4/data/10.4.0/RealSurface2.1
 export G4SAIDXSDATA=/opt/geant4/data/10.4.0/G4SAIDDATA1.1
-export G4ABLADATA=/opt/geant4/data/10.4.0/G4ABLA3.1
-export G4ENSDFSTATEDATA=/opt/geant4/data/10.4.0/G4ENSDFSTATE2.2
+export G4LEVELGAMMADATA=/opt/geant4/data/10.4.0/PhotonEvaporation5.2
+export G4RADIOACTIVEDATA=/opt/geant4/data/10.4.0/RadioactiveDecay5.2
+export G4REALSURFACEDATA=/opt/geant4/data/10.4.0/RealSurface2.1
 #
 cat << EOD > config.json5
 {
