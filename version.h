d5f91325d4ce2337d99c8bfdac3894480b2d9f2c
Koichi Murakami <koichi.murakami@kek.jp>
2020-04-08 15:53:16 +0900

    version updated

--- version.h
+++ version.h
@@ -13,7 +13,7 @@
 
 #define G4BENCH_VERSION_MAJOR "1"
 #define G4BENCH_VERSION_MINOR "5.3"
-#define G4BENCH_VERSION_BUILD 0x52a484e
+#define G4BENCH_VERSION_BUILD 0xec9b5c3
 
 namespace {
   const int build_head = (G4BENCH_VERSION_BUILD & 0xffff000) >> 12;
