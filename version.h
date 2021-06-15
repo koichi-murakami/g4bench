9ca53c48b330507a8f2b45414937bd9c9b095bc0
Koichi Murakami <koichi.murakami@kek.jp>
2021-06-15 10:54:00 +0900

    set version to 1.6.2

--- version.h
+++ version.h
@@ -12,8 +12,8 @@
 #define VERSION_H_
 
 #define G4BENCH_VERSION_MAJOR "1"
-#define G4BENCH_VERSION_MINOR "6.1.1"
-#define G4BENCH_VERSION_BUILD 0x26f40dd
+#define G4BENCH_VERSION_MINOR "6.2"
+#define G4BENCH_VERSION_BUILD 0x6bc7217
 
 namespace {
   const int build_head = (G4BENCH_VERSION_BUILD & 0xffff000) >> 12;
