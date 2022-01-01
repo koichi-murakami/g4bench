fd19fd5abdb206132f1bbba68390335f10c85a00
Koichi Murakami <koichi.murakami@kek.jp>
2022-01-01 12:47:00 +0900

    add cpu info and bench name for run

--- bench/mtbench.sh
+++ bench/mtbench.sh
@@ -20,6 +20,13 @@ app=$1
 uevent=$2
 log=$3
 
+sys=`uname`
+if [ ${sys} = "Darwin" ]; then
+  cpu_info=`sysctl machdep.cpu.brand_string | cut -d : -f 2 | xargs echo`
+else
+  cpu_info=`lscpu | grep name | cut -d : -f 2 | xargs echo`
+fi
+
 # ======================================================================
 echo "thread, eps, edep" > $log.dat
 
@@ -27,7 +34,7 @@ for t in "${th_list[@]}"
 do
   echo "running... #threads = $t"
   nevent=`expr $uevent \* $t`
-  $app -n $t -j $nevent > $log-n$t.log 2>&1
+  $app -n $t -j $nevent -p "${cpu_info}" -b $log > $log-n$t.log 2>&1
   echo -n "$t,  " >> $log.dat
   sed -n 2p jtest.out >> $log.dat
   mv g4bench.json $log-n$t.json
